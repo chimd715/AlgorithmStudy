487c374f0c348e3eb7a3c4cb42596f4b4fdb9c6c
hjh <chimd715@naver.com>
2021-10-21 16:47:46 +0900

    feat: programmers divisor

---
+++ Programmers/level1/divisor.py
@@ -0,0 +1,36 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.18
+    url: https://programmers.co.kr/learn/courses/30/lessons/77884
+"""
+from common import print_solved
+
+
+def find_all_divisors(number):
+    divisors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            divisors.append(i)
+    return divisors
+
+
+def solution(left, right):
+    answer = 0
+    for i in range(left, right + 1):
+        divisors = find_all_divisors(i)
+        answer += i if len(divisors) % 2 == 0 else -1 * i
+    return answer
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution(13, 17),
+        solution(24, 27)
+    ]
+
+    answers = [
+        43,
+        52
+    ]
+
+    print_solved(solutions, answers)
