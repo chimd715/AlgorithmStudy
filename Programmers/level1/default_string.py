fc5523e372bb004b9c6def708f494102f6c4bab4
hjh <chimd715@naver.com>
2021-10-21 17:21:29 +0900

    feat: programmers default string

---
+++ Programmers/level1/default_string.py
@@ -0,0 +1,27 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.21
+    url: https://programmers.co.kr/learn/courses/30/lessons/12918
+"""
+from common import print_solved
+import re
+
+
+def solution(s):
+    if len(s) not in [4, 6]:
+        return False
+    return False if re.search(r'[^0-9]', s) else True
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution("a234"),
+        solution("1324")
+    ]
+
+    answers = [
+        False,
+        True
+    ]
+
+    print_solved(solutions, answers)
