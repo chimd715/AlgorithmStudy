bc5e9211b4f8a9c957e53f6f0d1239455a88ff74
hjh <chimd715@naver.com>
2021-10-22 19:02:16 +0900

    feat: programmers rectangle star

---
+++ Programmers/level1/rectangle_star.py
@@ -0,0 +1,16 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.22
+    url: https://programmers.co.kr/learn/courses/30/lessons/12969
+"""
+from common import print_solved
+
+
+def solution(a, b):
+    # a, b = map(int, input().strip().split(' '))
+    for line in ['*' * a] * b:
+        print(line)
+
+
+if __name__ == "__main__":
+    solution(5, 3)
