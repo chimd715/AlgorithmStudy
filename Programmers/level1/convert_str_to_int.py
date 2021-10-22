851fbbb94e20aaa48da770f5ed83339da4650e80
hjh <chimd715@naver.com>
2021-10-22 12:38:59 +0900

    feat: programmers convert str to int

---
+++ Programmers/level1/convert_str_to_int.py
@@ -0,0 +1,24 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.22
+    url: https://programmers.co.kr/learn/courses/30/lessons/12925
+"""
+from common import print_solved
+
+
+def solution(s):
+    return int(s)
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution("1234"),
+        solution("-1234")
+    ]
+
+    answers = [
+        1234,
+        -1234
+    ]
+
+    print_solved(solutions, answers)
