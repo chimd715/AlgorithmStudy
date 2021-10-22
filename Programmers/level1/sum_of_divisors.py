051a40e2ca1412e1c471a93f737725413ee6aa8f
hjh <chimd715@naver.com>
2021-10-22 17:14:59 +0900

    feat: programmers sum of divisors

---
+++ Programmers/level1/sum_of_divisors.py
@@ -0,0 +1,28 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.22
+    url: https://programmers.co.kr/learn/courses/30/lessons/12928
+"""
+from common import print_solved
+
+
+def find_divisors(n):
+    return [i for i in range(1, n+1) if n % i ==0]
+
+
+def solution(n):
+    return sum(find_divisors(n))
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution(12),
+        solution(5)
+    ]
+
+    answers = [
+        28,
+        6
+    ]
+
+    print_solved(solutions, answers)
