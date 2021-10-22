e1c8c0d8e295c6ea312fc3599dbdd128b0e8be11
hjh <chimd715@naver.com>
2021-10-22 12:39:43 +0900

    feat: programmers descending int

---
+++ Programmers/level1/descending_int.py
@@ -0,0 +1,22 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.22
+    url: https://programmers.co.kr/learn/courses/30/lessons/12933
+"""
+from common import print_solved
+
+
+def solution(n):
+    return int(''.join(sorted([num for num in str(n)], key=lambda x: int(x), reverse=True)))
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution(118372)
+    ]
+
+    answers = [
+        873211
+    ]
+
+    print_solved(solutions, answers)
