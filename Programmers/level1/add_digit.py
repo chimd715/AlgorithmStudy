695389c8e3ea28f6bc0f867053beeac3c82afdf3
hjh <chimd715@naver.com>
2021-10-22 12:39:24 +0900

    feat: programmers add digit

---
+++ Programmers/level1/add_digit.py
@@ -0,0 +1,24 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.22
+    url: https://programmers.co.kr/learn/courses/30/lessons/12931
+"""
+from common import print_solved
+
+
+def solution(n):
+    return sum([int(num) for num in str(n)])
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution(123),
+        solution(987)
+    ]
+
+    answers = [
+        6,
+        24
+    ]
+
+    print_solved(solutions, answers)
