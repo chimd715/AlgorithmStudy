988aacd26533df3549ceae81fc0534f3be64ddd7
hjh <chimd715@naver.com>
2021-10-22 17:22:34 +0900

    feat: programmers root of int

---
+++ Programmers/level1/root_of_int.py
@@ -0,0 +1,28 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.22
+    url: https://programmers.co.kr/learn/courses/30/lessons/12934
+"""
+from common import print_solved
+from math import sqrt
+
+
+def solution(n):
+    float_sqrt = int(sqrt(n))
+    if float_sqrt * float_sqrt == n:
+        return (float_sqrt + 1) * (float_sqrt + 1)
+    return -1
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution(121),
+        solution(3)
+    ]
+
+    answers = [
+        144,
+        -1
+    ]
+
+    print_solved(solutions, answers)
