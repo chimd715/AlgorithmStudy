e3d250975010eea4a0aa1c2a6b3d6497918999b9
hjh <chimd715@naver.com>
2021-10-23 11:36:35 +0900

    feat: programmers create strange string

---
+++ Programmers/level1/create_strange_string.py
@@ -0,0 +1,28 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.23
+    url: https://programmers.co.kr/learn/courses/30/lessons/12930
+"""
+from common import print_solved
+
+
+def solution(s):
+    answer = []
+    for word in s.split(' '):
+        new_word = ''
+        for i, char in enumerate(word):
+            new_word += char.upper() if i % 2 == 0 else char.lower()
+        answer.append(new_word)
+    return ' '.join(answer)
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution("try hello world")
+    ]
+
+    answers = [
+        "TrY HeLlO WoRlD"
+    ]
+
+    print_solved(solutions, answers)
