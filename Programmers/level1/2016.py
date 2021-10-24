44a0da8f3dbf886f5c17f31c710235391b264e27
hjh <chimd715@naver.com>
2021-10-24 15:17:14 +0900

    feat: programmers 2016

---
+++ Programmers/level1/2016.py
@@ -0,0 +1,30 @@
+"""
+    programmers algorithm
+    last solved: 2021.10.24
+    url: https://programmers.co.kr/learn/courses/30/lessons/12901
+"""
+from common import print_solved
+
+
+def solution(a, b):
+    from datetime import date
+    return date(2016, a, b).ctime()[:3].upper()
+
+
+def solution_2(a, b):
+    # 각 달마다 며칠까지 있는지 안다고 가정
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    return days[(sum(months[:a-1]) + b-1) % 7]
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution(5, 24)
+    ]
+
+    answers = [
+        "TUE"
+    ]
+
+    print_solved(solutions, answers)
