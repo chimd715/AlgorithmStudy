e2104184ca8d5aa13c2adcd074b7f2455bf00c20
hjh <chimd715@naver.com>
2021-10-27 23:16:06 +0900

    feat: common algorithm file add

---
+++ algorithm.py
@@ -0,0 +1,22 @@
+"""
+    programmers algorithm
+    last solved:
+    url:
+"""
+from common import print_solved
+
+
+def solution():
+    return None
+
+
+if __name__ == "__main__":
+    solutions = [
+        solution()
+    ]
+
+    answers = [
+        None
+    ]
+
+    print_solved(solutions, answers)
