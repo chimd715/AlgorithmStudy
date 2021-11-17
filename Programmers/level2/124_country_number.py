3ec62152c05cf1e99f03d993f568f0971a718ba7
hjh <chimd715@naver.com>
2021-11-17 16:57:38 +0900

    feat: programmers 124 country number - simpler first solution

--- Programmers/level2/124_country_number.py
+++ Programmers/level2/124_country_number.py
@@ -17,6 +17,15 @@ def solution(n):
     return based_num
 
 
+def solution_2(n):
+    rebase = ["1", "2", "4"]
+    based_num = ""
+    while n > 0:
+        n, remainder = divmod(n-1, 3)
+        based_num = str(rebase[remainder]) + based_num
+    return based_num
+
+
 if __name__ == "__main__":
     solutions = [
         solution(1),
